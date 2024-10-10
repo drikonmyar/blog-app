9642996adc44b074dadfc7dd88e6b7e5480df73f
Nabyendu Ojha <nabyenduojha99@gmail.com>
2024-10-11 01:07:00 +0530

    added image processing

--- blog-backend/src/main/java/com/backend/blog/controller/PostController.java
+++ blog-backend/src/main/java/com/backend/blog/controller/PostController.java
@@ -1,15 +1,24 @@
 package com.backend.blog.controller;
 
 import com.backend.blog.config.AppConstants;
+import com.backend.blog.entity.Post;
 import com.backend.blog.payload.ApiResponse;
 import com.backend.blog.payload.PostDto;
 import com.backend.blog.payload.PostResponse;
+import com.backend.blog.service.FileService;
 import com.backend.blog.service.PostService;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
 @RestController
@@ -18,6 +27,12 @@ public class PostController {
 
     @Autowired
     private PostService postService;
+
+    @Autowired
+    private FileService fileService;
+
+    @Value("${project.image}")
+    private String path;
 
     @PostMapping("/user/{userId}/category/{categoryId}")
     public ResponseEntity<PostDto> createPost(@RequestBody PostDto postDto, @PathVariable Integer userId, @PathVariable Integer categoryId){
@@ -70,6 +85,28 @@ public class PostController {
     public ResponseEntity<List<PostDto>> searchPosts(@PathVariable String keyword){
         List<PostDto> searchPosts = this.postService.searchPosts(keyword);
         return new ResponseEntity<>(searchPosts, HttpStatus.OK);
+    }
+
+    @PostMapping("/image/upload/{postId}")
+    public ResponseEntity<PostDto> uploadPostImage(
+            @RequestParam("image") MultipartFile image,
+            @PathVariable Integer postId
+            ) throws IOException {
+        PostDto postDto = this.postService.getPostById(postId);
+        String fileName = this.fileService.uploadImage(path, image);
+        postDto.setImageName(fileName);
+        PostDto newPostDto = this.postService.updatePost(postDto, postId);
+        return new ResponseEntity<>(newPostDto, HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/image/{imageName}", produces = MediaType.IMAGE_JPEG_VALUE)
+    public void downloadImage(
+            @PathVariable String imageName,
+            HttpServletResponse response
+    ) throws IOException {
+        InputStream resource = this.fileService.getResource(path, imageName);
+        response.setContentType(MediaType.IMAGE_JPEG_VALUE);
+        StreamUtils.copy(resource, response.getOutputStream());
     }
 
 }
---
+++ blog-backend/src/main/java/com/backend/blog/service/impl/FileServiceImpl.java
@@ -0,0 +1,33 @@
+package com.backend.blog.service.impl;
+
+import com.backend.blog.service.FileService;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.UUID;
+
+@Service
+public class FileServiceImpl implements FileService {
+    @Override
+    public String uploadImage(String path, MultipartFile file) throws IOException {
+        String name = file.getOriginalFilename();
+        String randomId = UUID.randomUUID().toString();
+        String fileName = randomId.concat(name.substring(name.lastIndexOf(".")));
+        String filePath = path + File.separator + fileName;
+        File f = new File(path);
+        if(!f.exists()){
+            f.mkdir();
+        }
+        Files.copy(file.getInputStream(), Paths.get(filePath));
+        return fileName;
+    }
+
+    @Override
+    public InputStream getResource(String path, String fileName) throws FileNotFoundException {
+        String fullPath = path + File.separator + fileName;
+        return new FileInputStream(fullPath);
+    }
+}
